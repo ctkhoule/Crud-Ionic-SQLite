a3570904a56652adc7b77af1ea2c422d93b50db5
ctk <ctkhoule@gmail.com>
2020-06-07 12:31:27 +0000

    Implémentation SQLite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+/*Importer SQLite*/
+import { SQLite } from '@ionic-native/sqlite/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
+      SQLite,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {SQLite, SQLiteObject} from "@ionic-native/sqlite";
+import {platform} from "os";
+import {Platform} from "@ionic/angular";
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,20 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  //Définition nom BD et table
+  databaseObj: SQLiteObject;
+  readonly database_name: string = "freaky_database.db";
+  readonly table_name: string = "myfreakytable";
+
+  name_modl: string = "";
+  row_data: any = [];
+
+  //Operation de MAJ
+  updateActive: boolean;
+  to_update_item:any;
+
+  constructor(private platform: Platform, private sqlite: SQLite) {
+
+  }
 
 }
